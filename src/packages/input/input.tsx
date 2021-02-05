bebc6c732933f3c0ac92dfdaac745848640d49cd
yesyb <nicaiwho@163.com>
2021-02-05 13:51:27 +0800

    compoent:input add clear and focus function

--- src/packages/input/input.tsx
+++ src/packages/input/input.tsx
@@ -14,9 +14,19 @@ export default defineComponent({
             'pl-input',
             `pl-input-status-${props.status}`
         ]);
+        const methods = {
+            focus:()=>{
+                inputRef.value!.focus();
+            },
+            clear:()=>{
+                modelValue.value = '';
+            }
+        }
         return () => (
             <div class={classes.value}>
                 <input class="pl-input-inner" type="text" v-model={modelValue.value} ref={inputRef}/>
+                <button onClick={methods.clear}>Clear</button>
+                <button onClick={methods.focus}>Focus</button>
             </div>
         )
     }
